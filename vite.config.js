b576fff483be3fd304b8128eb73452ad9fe75b0d
EfrenBarCast <name@email.comgit config --global user.email>
2026-07-14 09:18:43 -0500

    feat: add fix index

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'; 
 import svgr from 'vite-plugin-svgr';
-import { VitePWA } from 'vite-plugin-pwa'; // 👈 Agregamos esta importación
+import { VitePWA } from 'vite-plugin-pwa'; 
 
 export default defineConfig({
   plugins: [
     react(), 
     svgr(),
     VitePWA({ 
-      registerType: 'Simon Movilidad',
+      registerType: 'autoUpdate', 
       manifest: {
         name: 'Sala de control',
-        short_name: 'MiApp',
+        short_name: 'Simon Movilidad', 
         description: 'Control vehicular inteligente',
         theme_color: '#ffffff',
         icons: [
           {
             src: '/favicon.svg', 
             sizes: '192x192',
-            type: 'image/png'
+            type: './imagenes/logo-simon-movilidad.svg'
           },
           {
             src: '/favicon.svg',
             sizes: '512x512',
-            type: 'image/png'
+            type: './imagenes/logo-simon-movilidad.svg' 
           }
         ]
       }
